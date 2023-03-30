18d87cea96e615ff6d2fb5387019cca27cf0e603
Apurba Koley <btech.i1_44583@apsc.mait.ac.in>
2023-03-31 01:56:17 +0530

    Fixed a bug which was caused during login

--- src/context/StateFiles/ProfileState.jsx
+++ src/context/StateFiles/ProfileState.jsx
@@ -45,7 +45,10 @@ const ProfileState = (props)=> {
 
 
     }
-    getLoggedinUserData()
+    if(window.location.pathname!=='/' && window.location.pathname!=='/login' &&window.location.pathname!=='/signup') {
+
+      getLoggedinUserData()
+    }
 
   }, [])
 
